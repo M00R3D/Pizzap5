aeb3bc76e5983631c34814c5dd7006beed1e0f9b
M00R3D <jobmurdan@hotmail.com>
2025-05-31 20:39:54 -0700

    Se completa la simulación con la tercera pizza usando el algoritmo de Bresenham.

--- main.js
+++ main.js
@@ -21,17 +21,21 @@ function draw() {
   translate(400, 0);
   dibujarPizzaDDA(divisiones);
   pop();
+
+  push();
+  translate(650, 0);
+  dibujarPizzaBresenham(divisiones);
+  pop();
 }
 
 function dibujarPizzaPendiente(n) {
-  let radio = 100;
+  let r = 100;
   fill(255, 200, 150);
-  ellipse(0, 0, radio * 2);
-
+  ellipse(0, 0, r * 2);
   for (let i = 0; i < n; i++) {
-    let angulo = TWO_PI * i / n;
-    let x = round(radio * cos(angulo));
-    let y = round(radio * sin(angulo));
+    let a = TWO_PI * i / n;
+    let x = round(r * cos(a));
+    let y = round(r * sin(a));
     lineaPendiente(0, 0, x, y);
   }
 }
@@ -41,7 +45,6 @@ function lineaPendiente(x0, y0, x1, y1) {
   let dy = y1 - y0;
   let m = dy / dx;
   let b = y0 - m * x0;
-
   if (abs(dx) > abs(dy)) {
     let paso = dx > 0 ? 1 : -1;
     for (let x = x0; x != x1; x += paso) {
@@ -58,14 +61,13 @@ function lineaPendiente(x0, y0, x1, y1) {
 }
 
 function dibujarPizzaDDA(n) {
-  let radio = 100;
+  let r = 100;
   fill(255, 200, 150);
-  ellipse(0, 0, radio * 2);
-
+  ellipse(0, 0, r * 2);
   for (let i = 0; i < n; i++) {
-    let angulo = TWO_PI * i / n;
-    let x = round(radio * cos(angulo));
-    let y = round(radio * sin(angulo));
+    let a = TWO_PI * i / n;
+    let x = round(r * cos(a));
+    let y = round(r * sin(a));
     lineaDDA(0, 0, x, y);
   }
 }
@@ -78,10 +80,43 @@ function lineaDDA(x0, y0, x1, y1) {
   let incY = dy / pasos;
   let x = x0;
   let y = y0;
-
   for (let i = 0; i <= pasos; i++) {
     point(round(x), round(y));
     x += incX;
     y += incY;
+  }
+}
+
+function dibujarPizzaBresenham(n) {
+  let r = 100;
+  fill(255, 200, 150);
+  ellipse(0, 0, r * 2);
+  for (let i = 0; i < n; i++) {
+    let a = TWO_PI * i / n;
+    let x = round(r * cos(a));
+    let y = round(r * sin(a));
+    lineaBresenham(0, 0, x, y);
+  }
+}
+
+function lineaBresenham(x0, y0, x1, y1) {
+  let dx = abs(x1 - x0);
+  let dy = abs(y1 - y0);
+  let sx = x0 < x1 ? 1 : -1;
+  let sy = y0 < y1 ? 1 : -1;
+  let err = dx - dy;
+
+  while (true) {
+    point(x0, y0);
+    if (x0 === x1 && y0 === y1) break;
+    let e2 = 2 * err;
+    if (e2 > -dy) {
+      err -= dy;
+      x0 += sx;
+    }
+    if (e2 < dx) {
+      err += dx;
+      y0 += sy;
+    }
   }
 }
