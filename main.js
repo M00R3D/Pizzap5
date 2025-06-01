c45ae0de1fb2816da3bb02110b308b7a99305c55
M00R3D <jobmurdan@hotmail.com>
2025-05-31 20:39:22 -0700

    Se agrega la segunda pizza con algoritmo DDA para dividir en rebanadas iguales.

--- main.js
+++ main.js
@@ -10,8 +10,17 @@ function setup() {
 
 function draw() {
   background(255);
-  translate(150, height / 2);
+  translate(0, height / 2);
+
+  push();
+  translate(150, 0);
   dibujarPizzaPendiente(divisiones);
+  pop();
+
+  push();
+  translate(400, 0);
+  dibujarPizzaDDA(divisiones);
+  pop();
 }
 
 function dibujarPizzaPendiente(n) {
@@ -45,5 +54,34 @@ function lineaPendiente(x0, y0, x1, y1) {
       let x = round((y - b) / m);
       point(x, y);
     }
+  }
+}
+
+function dibujarPizzaDDA(n) {
+  let radio = 100;
+  fill(255, 200, 150);
+  ellipse(0, 0, radio * 2);
+
+  for (let i = 0; i < n; i++) {
+    let angulo = TWO_PI * i / n;
+    let x = round(radio * cos(angulo));
+    let y = round(radio * sin(angulo));
+    lineaDDA(0, 0, x, y);
+  }
+}
+
+function lineaDDA(x0, y0, x1, y1) {
+  let dx = x1 - x0;
+  let dy = y1 - y0;
+  let pasos = max(abs(dx), abs(dy));
+  let incX = dx / pasos;
+  let incY = dy / pasos;
+  let x = x0;
+  let y = y0;
+
+  for (let i = 0; i <= pasos; i++) {
+    point(round(x), round(y));
+    x += incX;
+    y += incY;
   }
 }
